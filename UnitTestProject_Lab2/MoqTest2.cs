7cc1754acb9f4a70034cca86df8028912d0ac400
Monia <monika.nabielec@gmail.com>
2018-12-07 22:32:26 +0100

    .

--- UnitTestProject_Lab2/MoqTest2.cs
+++ UnitTestProject_Lab2/MoqTest2.cs
@@ -18,5 +18,21 @@ namespace UnitTestProject_Lab2
             service.PlaceOrder(order);
             storage.Verify(s => s.Store(order), Times.Once);
         }
+
+
+        [Test]
+        public void TestOrderIdWhenObjectIsAdded
+            ()
+        {
+            Mock<IStorage> _storageMock = new Mock<IStorage>();
+            var _order = new Order();
+            var _service = new OrderService(_storageMock.Object);
+            _storageMock.Setup(m => m.Store(2).CompareTo(2));
+            
+
+            //when false
+
+            //_storageMock.Setup(s => s.Store(null)).Throws(new NullPointerException());
+        }
     }
 }
